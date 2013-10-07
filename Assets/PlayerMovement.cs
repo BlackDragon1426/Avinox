4292d66bc154bdf67ef205cb8819b2b1c17c2b3e
BlackDragon1426 <blackdragon1426@gmail.com>
2013-10-07 14:16:36 -0400

    Started Scripting the player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	float speed = 10;
+	
+	void Start ()
+	{
+	}
+	
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+	
+		Vector3 direction = new Vector3(h,0.0f,v);
+		transform.Translate(direction * speed * Time.deltaTime);
+		
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.AddForce(Vector3.up * 5.0f,ForceMode.VelocityChange);	
+		}
+		
+	}
+}
