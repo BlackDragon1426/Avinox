639e6df8fbb0f3b21caf9a0f0850df9b7e214090
Anthony Chaisson <blackdragon1426@gmail.com>
2013-10-11 12:02:20 -0400

    More Player Changes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,28 +3,43 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-	float speed = 10;
+	float walkSpeed = 3;
+	float jogSpeed = 6;
+	float sprintSpeed = 10;
 	
-	void Start ()
-	{
-	}
+	float flightSpeed;
+	
+	bool grounded = false;
+	
+	public float currentSpeed;
 	
 	void FixedUpdate ()
 	{
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
+		
+		if(Input.GetKey(KeyCode.LeftShift))
+		{
+			currentSpeed = Mathf.Lerp(jogSpeed, sprintSpeed, Time.time / 2f);
+		}
+		else
+		{
+			currentSpeed = jogSpeed;
+		}
 	
 		Vector3 direction = new Vector3(h,0.0f,v);
-		transform.Translate(direction * speed * Time.deltaTime);
+		transform.Translate(direction * currentSpeed * Time.deltaTime);
 		
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Space) && grounded == true)
 		{
 			rigidbody.AddForce(Vector3.up * 5.0f,ForceMode.VelocityChange);	
 		}
 		
-		Vector3 movespeed;
 		
-//		Debug.Log(movespeed);
 	}
 	
+	void GroundedCheck()
+	{
+		
+	}
 }
