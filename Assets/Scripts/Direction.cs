e8da8717dd515dcca7dcb5a1d0a06e39ec70aa18
BlackDragon1426 <blackdragon1426@gmail.com>
2014-01-30 19:19:35 -0500

    zcxvzxcvzc

--- Assets/Scripts/Direction.cs
+++ Assets/Scripts/Direction.cs
@@ -8,31 +8,40 @@ public class Direction : MonoBehaviour
 	void FixedUpdate()
 	{
 		rot = 0;
+//
+//		if(Input.GetAxis("MoveY") >= 0)
+//		{
+//			if(Input.GetAxis("MoveX") > 0)
+//			{
+//				rot = Input.GetAxis("MoveX") * 90;
+//			}
+//			else if(Input.GetAxis("MoveX") < 0)
+//			{
+//				rot = Input.GetAxis("MoveX") * 90;
+//			}
+//		}
+//		else if(Input.GetAxis("MoveY") <= 0)
+//		{
+//			rot = 180;
+//
+//			if(Input.GetAxis("MoveX") > 0)
+//			{
+//				rot += Input.GetAxis("MoveX") * -1 * 90;
+//			}
+//			else if(Input.GetAxis("MoveX") < 0)
+//			{
+//				rot += Input.GetAxis("MoveX") * -1 * 90;
+//			}
+//		}
 
-		if(Input.GetAxis("MoveY") >= 0)
-		{
-			if(Input.GetAxis("MoveX") > 0)
-			{
-				rot = Input.GetAxis("MoveX") * 90;
-			}
-			else if(Input.GetAxis("MoveX") < 0)
-			{
-				rot = Input.GetAxis("MoveX") * 90;
-			}
-		}
-		else if(Input.GetAxis("MoveY") <= 0)
-		{
-			rot = 180;
+		Vector2 joystickOrigin = new Vector2(0,0);
 
-			if(Input.GetAxis("MoveX") > 0)
-			{
-				rot += Input.GetAxis("MoveX") * -1 * 90;
-			}
-			else if(Input.GetAxis("MoveX") < 0)
-			{
-				rot += Input.GetAxis("MoveX") * -1 * 90;
-			}
-		}
+		Vector2 input = new Vector2(Input.GetAxis("MoveX"),Input.GetAxis("MoveY"));
+
+		Vector2 straight = new Vector2(0,1);
+
+
+
 		Debug.Log(rot);
 	}
 }
